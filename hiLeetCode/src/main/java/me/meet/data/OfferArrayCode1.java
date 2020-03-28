fe3023b113e50c47d45858b1d7ea4e98cbacdad4
zhangxudong8 <zhangxudong8@guazi.com>
2020-03-28 18:07:22 +0800

    add OfferArrayCode1

--- hiLeetCode/src/main/java/me/meet/data/OfferArrayCode1.java
+++ hiLeetCode/src/main/java/me/meet/data/OfferArrayCode1.java
@@ -188,10 +188,63 @@ public final class OfferArrayCode1 {
         System.out.println(res);
     }
 
+    /**
+     * 调整数组顺序使奇数位于偶数前面
+     *
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+     * 使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，
+     * 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+     *
+     * 思路:
+     * 1.要想保证原有次序，则只能顺次移动或相邻交换。
+     * 2.i从左向右遍历，找到第一个偶数。
+     * 3.j从i+1开始向后找，直到找到第一个奇数。
+     * 4.将[i,...,j-1]的元素整体后移一位，最后将找到的奇数放入i位置，然后i++。
+     * 5.终止条件，i || j 越界
+     *
+     * 说明：
+     * 本方法每次推进多次元素，性能较高
+     */
+    static void reOrderArrayII(int[] array) {
+        if (array == null || array.length < 1) {
+            return;
+        }
+        int i = 0, j = 0, len = array.length;
+        for (; i < len && j < len; ) {
+            // 找到第一个奇数
+            while (i < len && (array[i] & 1) == 1) {
+                i++;
+            }
+            j = i + 1;
+            // 找到之后的第一个偶数
+            while (j < array.length && (array[j] & 1) == 0) {
+                j++;
+            }
+            // 中间的元素后移
+            if (j < len) {
+                int tmp = array[j];
+                for (int k = j - 1; k >= i; k--) {
+                    array[k + 1] = array[k];
+                }
+                array[i++] = tmp;
+            }
+        }
+    }
+
+    private static void testReOrderArrayII() {
+        int[] arr = {1, 2, 3, 4, 5, 7};
+        reOrderArrayII(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         testMedianNumber();
         testFindDuplicate();
         testCountN();
         testTwoDimensionalArrayLookup();
+        testReOrderArrayII();
     }
 }
