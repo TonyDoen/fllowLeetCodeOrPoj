174e42ff336eb90b5b34d9110d468d158c0d0d90
zhangxudong1 <zhangxudong1@xiaomi.com>
2021-01-14 21:55:56 +0800

    add LC111

--- hiLeetCode/src/main/java/me/meet/labuladong/common/TreeNode.java
+++ hiLeetCode/src/main/java/me/meet/labuladong/common/TreeNode.java
@@ -71,6 +71,22 @@ public class TreeNode {
         return _1;
     }
 
+    public static TreeNode prepareTree2() {
+        /**
+         *         3
+         *       /   \
+         *     9     20
+         *           /  \
+         *          15   7
+         */
+        TreeNode _15 = new TreeNode(15);
+        TreeNode _7 = new TreeNode(7);
+        TreeNode _20 = new TreeNode(20, _15, _7);
+        TreeNode _9 = new TreeNode(9);
+        TreeNode _3 = new TreeNode(3, _9, _20);
+        return _3;
+    }
+
     public int maxLevel() {
         return maxLevel(this);
     }
